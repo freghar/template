6553cf95061101dd981fe3f23a228cffa432e82b
freghar <freghar@dummy.tld>
2016-03-28 00:49:58 +0200

    improve include structure, DEBUG, userconfig.h

--- functions/Example/fn_sleepFor.sqf
+++ functions/Example/fn_sleepFor.sqf
@@ -1,8 +1,14 @@
+#include "..\..\include\common.h"
+
 /*
  * pause execution in a scheduled environment for '_secs' seconds specified,
  * return immediately in a non-scheduled environment
  */
 
 params ["_secs"];
+
+#ifdef DEBUG
+hint format ["sleeping for %1 seconds", _secs];
+#endif
 
 sleep _secs;
---
+++ userconfig.h
@@ -0,0 +1,19 @@
+/*
+ * this is a template user configuration file, to be edited by the mission
+ * creator, tuning the template for a specific mission style
+ */
+
+/*
+ * Generic features
+ */
+
+/*
+ * Gameplay style specifics
+ */
+
+/*
+ * Misc
+ */
+
+/* various debugging options, might add extra verbosity */
+//#define DEBUG
